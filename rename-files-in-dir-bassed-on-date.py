aecadbb5fd27c1053cf5d33a78cbcc75585233ac
p-a <peter.augustinak@gmail.com>
2021-09-24 15:05:34 +0200

    Final fixes, change to callable function with dir and name dict as params

--- rename-files-in-dir-bassed-on-date.py
+++ rename-files-in-dir-bassed-on-date.py
@@ -2,31 +2,34 @@ import os
 import os.path
 import time
 
+def rename_files_in_dir_based_on_date(path_to_dir, names):
+
+    my_dir = os.chdir(path_to_dir)
+    files = os.listdir(my_dir)
+
+    index = 0
+    current_file_date = ''
+
+    for file in files:
+        file_date = ' '.join(time.ctime(os.path.getmtime(file)).split()[1:3])
+        if file_date in names.keys():
+            # if there is different date on a file as previous one, the renaming name must be different, so also index count is reseted
+            if file_date != current_file_date:
+                index = 0
+
+            index = int(index) + 1
+            index = f'0{index}' if index < 10 else index
+            
+            current_file_date = file_date
+            new_file_name = f'{names[file_date]}{index}.mp4'
+
+            os.rename(file, new_file_name)
+
+    # show result
+    for file in os.listdir():
+        print(file)
+
 
 path_to_dir = 'D:/video/sntdo/source/'
-my_dir = os.chdir(path_to_dir)
-
-names = {'Sep 26': 'garaz-', 'Sep 5': 'na-kupko-', 'Sep 19': 'do-karanteny-', 'Aug 23': 'do-dazda-', 'Sep 18': 'do-prace-'}
-files = os.listdir(my_dir)
-
-index = 0
-current_file_date = ''
-
-for file in files:
-    file_date = ' '.join(time.ctime(os.path.getmtime(file)).split()[1:3])
-    if file_date in names.keys():
-        # if there is different date on a file as previous one, the renaming name must be different, so also index count is reseted
-        if file_date != current_file_date:
-            index = 0
-
-        index = int(index) + 1
-        index = f'0{index}' if index < 10 else index
-        
-        current_file_date = file_date
-        new_file_name = f'{names[file_date]}{index}.mp4'
-
-        print(new_file_name)
-        #os.rename(os.path.join(my_dir, file), os.path.join(my_dir, new_file_name))
-
-#print(os.listdir())
-
+names = {'Sep 26': 'garaz-', 'Sep 5': 'na-kupko-', 'Sep 19': 'hrat-fotbal-', 'Aug 23': 'do-dazda-', 'Sep 18': 'do-prace-'}
+rename_files_in_dir_based_on_date(path_to_dir, names)
